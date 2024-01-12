9870bb9ca90582cb7579c77b8ef6041dcf04c743
bbeck9123 <byron.ptgo@gmail.com>
2024-01-12 06:53:20 -0800

    join

--- kyu8/lowerCase.js
+++ kyu8/lowerCase.js
@@ -3,8 +3,15 @@ Write a function which converts the input string to uppercase.
 Write a function which converts the input string to uppercase.
 
 
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+Example
+
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
 
+smash = function (words) {
+    return words.join(" ");
+  };
 
 
 
