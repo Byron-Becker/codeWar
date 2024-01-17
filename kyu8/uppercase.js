3edd343a2d05179d791da28d08b2e19ef8c3cb49
bbeck9123 <byron.ptgo@gmail.com>
2024-01-16 16:48:14 -0800

    repeat line

--- kyu8/uppercase.js
+++ kyu8/uppercase.js
@@ -31,3 +31,16 @@ function abbrevName(name){
     
     return `${first}.${second}`
   }
+
+  Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+function greet(name){
+ 
+    return `Hello, ${name} how are you doing today?` 
+     
+   }
+
+   
