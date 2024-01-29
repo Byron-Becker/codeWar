be802567be68b252cbcdd8f2325098f9851cf6da
bbeck9123 <byron.ptgo@gmail.com>
2024-01-29 11:00:29 -0800

    slice to make a copy

--- kyu7/sort.js
+++ kyu7/sort.js
@@ -14,3 +14,26 @@ All of the strings in the array passed to your function will be different length
 function sortByLength (array) {
     return array.sort((a,b) => a.length - b.length);
   };
+
+
+  The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme (triplet) {
+  
+    const midNum = triplet.slice().sort((a,b)=> a-b)[1]
+    
+    return triplet.indexOf(midNum)
+    
+  }
