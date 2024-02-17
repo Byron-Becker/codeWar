fd7c8ae1235267f7960d32bc91c31b2fe0fc42d7
bbeck9123 <byron.ptgo@gmail.com>
2024-02-17 11:27:25 -0800

    every limit

--- kyu6/numbers.js
+++ kyu6/numbers.js
@@ -19,3 +19,33 @@ function createPhoneNumber(numbers){
     }
     return numFormat
   }
+
+  The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+  Examples
+  
+  "din"      =>  "((("
+  "recede"   =>  "()()()"
+  "Success"  =>  ")())())"
+  "(( @"     =>  "))((" 
+  
+  Notes
+  
+  Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+  
+
+};
+  
+for(let i=0; i < word.length; i++){
+  let char = word[i];
+  
+  if(freqObj[char] > 1){
+    convStr += ')'
+  }else{
+    convStr += '('
+  };
+  
+};
+ 
+ return convStr
+ 
+};
