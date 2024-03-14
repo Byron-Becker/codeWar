9db6a474eecc5d858862d6d4d2b75086e5bbe2ae
bbeck9123 <byron.ptgo@gmail.com>
2024-03-14 16:27:11 -0700

    replace

--- kyu8/high.js
+++ kyu8/high.js
@@ -23,3 +23,21 @@ Notes
         return `${newArr[0]} ${newArr[newArr.length-1]}`
         
       };
+
+      This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+function accum(s) {
+    let arr = [];
+      for (let i = 0; i < s.length; i++) {
+      arr.push(s[i].toUpperCase() + s[i].toLowerCase().repeat(i));
+    }
+    return arr.join('-');
+  }
