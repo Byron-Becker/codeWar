3c69e01921538b3b2a75cef45e715fffc2ba71b3
bbeck9123 <byron.ptgo@gmail.com>
2024-03-14 16:13:07 -0700

    highlow

---
+++ kyu8/high.js
@@ -0,0 +1,25 @@
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Examples
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes
+
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+
+
+    function highAndLow(numbers){
+
+        //turn string into array, sort array from highest to lowest num
+        
+        let newArr = numbers.split(' ').sort((a,b)=>b-a)
+        
+        //take first and last element in array and return as a string with single space
+        
+        return `${newArr[0]} ${newArr[newArr.length-1]}`
+        
+      };
--- kyu8/jss.js
+++ kyu8/jss.js
@@ -55,3 +55,5 @@ function getMiddle(s)
   
   };
   
+
+  
