c29e63b0296c930e585ee9457183d5a55be59c17
bbeck9123 <byron.ptgo@gmail.com>
2024-03-15 14:21:31 -0700

    repeat

--- kyu8/lowerCase.js
+++ kyu8/lowerCase.js
@@ -47,6 +47,34 @@ smash = function (words) {
   }
 
 
+  An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+  Example: (Input --> Output)
+  
+  "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+  
+  isIsogram "Dermatoglyphics" = true
+  isIsogram "moose" = false
+  isIsogram "aba" = false
+  
+  function isIsogram(str){
+  
+    let newStr = str.toLowerCase()
+    
+    for(let i=0; i < str.length; i++){
+      for(let k=0; k < str.length; k++){
+        
+        if(i !== k && newStr[i] === newStr[k]){
+        return false
+          };
+      };
+      
+      
+    };
+    return true
+  };
+
+
 
 
 
