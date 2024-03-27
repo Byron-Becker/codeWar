2a8d6a9da7d8e4c8b3bd5a3589ee678180bd6af7
bbeck9123 <byron.ptgo@gmail.com>
2024-03-27 06:29:55 -0700

    odd-even

--- kyu7/cap.js
+++ kyu7/cap.js
@@ -41,3 +41,9 @@ function capitalize(s){
   
   return [evenStr, oddStr];
 };
+
+function capitalize(s){
+    const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+    const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+    return [even, odd];
+  };
